eca010d2760bc569ac176696146a3ee31236c113
KariAnn Yoshida <yoshidakariann@gmail.com>
2018-01-13 18:06:20 -0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-coffee" />
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Kari Ann Yoshida</h1>
+        <p>Full-Stack Web developer</p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Intro
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Work
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: React.PropTypes.func,
+  timeout: React.PropTypes.bool,
+}
+
+export default Header
